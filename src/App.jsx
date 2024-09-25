12a1e06c45ba374639df1dd35f05252b1bbd050a
Vikas Yadav <do.vikas1369@gmail.com>
2024-09-25 21:37:36 +0530

    Object destructuring in Component definition

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,12 @@ function Header(){//New component
     );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
     return (
       <li>
-          <img src={componentImg} alt={props.title}/>
-          <h3>{props.title}</h3>
-          <p>{props.description}</p>
+          <img src={image} alt={title}/>
+          <h3>{title}</h3>
+          <p>{description}</p>
       </li>
     );
 }
