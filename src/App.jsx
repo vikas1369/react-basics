2899c2243ec486cc300a3d17ea2fb88c6d7f280c
Vikas Yadav <do.vikas1369@gmail.com>
2024-10-03 08:55:15 +0530

    rendering components based on the state

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Header from './components/Header'
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 import {useState} from 'react';
+import {EXAMPLES} from "./data-with-examples";
 
 function App() {
     console.log("App rendered")
     //States are React variables which are bound to the Component that when changed causes the component to be re-rendered
-    const[selectedTopic, setSelectedTopic] = useState("Click on button");//useState should be called inside the Component function and should be at the top level not inside some function
+    const[selectedTopic, setSelectedTopic] = useState("components");//useState should be called inside the Component function and should be at the top level not inside some function
     function handleSelect(selectedButton){
         console.log("Button Selected");
         setSelectedTopic(selectedButton);
@@ -45,11 +46,17 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
-                        <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
-                        <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-                        <TabButton onSelect={() => handleSelect('TabButton')}>State</TabButton>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                     </menu>
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[selectedTopic].code}</code>
+                        </pre>
+                    </div>
                     {selectedTopic}
                 </section>
             </main>
